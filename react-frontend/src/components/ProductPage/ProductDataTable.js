4f0551041a9e8487565bc93203ad2bd023198b5c
Code Bridge Machine <ahmed@codebridge.my>
2024-02-24 10:29:03 +0800

    CodeBridge commit

---
+++ react-frontend/src/components/ProductPage/ProductDataTable.js
@@ -0,0 +1,30 @@
+
+import { Column } from 'primereact/column';
+import { DataTable } from 'primereact/datatable';
+import React, { useState } from 'react';
+import _ from 'lodash';
+import { Button } from 'primereact/button';
+
+
+const ProductDataTable = ({ items, onEditRow, onRowDelete, onRowClick }) => {
+    
+    const pTemplate0 = (rowData, { rowIndex }) => <p >{rowData.name}</p>
+    const pTemplate1 = (rowData, { rowIndex }) => <p >{rowData.Category}</p>
+    const pTemplate2 = (rowData, { rowIndex }) => <p >{rowData.SKU}</p>
+
+    const editTemplate = (rowData, { rowIndex }) => <Button onClick={() => onEditRow(rowData, rowIndex)} icon={`pi ${rowData.isEdit ? "pi-check" : "pi-pencil"}`} className={`p-button-rounded p-button-text ${rowData.isEdit ? "p-button-success" : "p-button-warning"}`} />;
+    const deleteTemplate = (rowData, { rowIndex }) => <Button onClick={() => onRowDelete(rowIndex)} icon="pi pi-times" className="p-button-rounded p-button-danger p-button-text" />;
+    
+    return (
+        <DataTable value={items} onRowClick={onRowClick} scrollable rowHover paginator rows={10} rowClassName="cursor-pointer">
+            <Column field="name" header="Name" body={pTemplate0} style={{ minWidth: "8rem" }} />
+            <Column field="Category" header="Category" body={pTemplate1} style={{ minWidth: "8rem" }} />
+            <Column field="SKU" header="SKU" body={pTemplate2} style={{ minWidth: "8rem" }} />
+
+            <Column header="Edit" body={editTemplate} />
+            <Column header="Delete" body={deleteTemplate} />
+        </DataTable>
+    );
+};
+
+export default ProductDataTable;
